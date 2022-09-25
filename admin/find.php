6280265efe3e15ff05e5812d59657a510e69d369
aahmad-147 <114324922+aahmad-147@users.noreply.github.com>
2022-09-25 15:22:23 +0700

    Update find.php

--- admin/find.php
+++ admin/find.php
@@ -2,19 +2,13 @@
 <html lang="en">
 
 <head>
-
     <title> LoveLove - Wedding & Wedding Planner HTML5 Template</title>
-
 </head>
 
 <body>
-
 <form action="../index.php" method="get">
- 
   <input type="text" id="name" name="name"><br><br>
- 
   <input type="submit" value="Submit">
 </form>
 </body>
-
 </html>
