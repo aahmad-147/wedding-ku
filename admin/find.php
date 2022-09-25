c12e82802408fd03fb52aa49613f9721b0b8b326
aahmad-147 <114324922+aahmad-147@users.noreply.github.com>
2022-09-25 15:19:07 +0700

    Add files via upload

---
+++ admin/find.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title> LoveLove - Wedding & Wedding Planner HTML5 Template</title>
+
+</head>
+
+<body>
+
+<form action="../index.php" method="get">
+ 
+  <input type="text" id="name" name="name"><br><br>
+ 
+  <input type="submit" value="Submit">
+</form>
+</body>
+
+</html>
